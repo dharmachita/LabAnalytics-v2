1e77cde050c048e45d4733868c02f922ee5b6039
Mauro Emmanuel Rambo <mauroemmanuel22@gmail.com>
2020-04-18 22:54:29 -0300

    First Commit

---
+++ resources/lang/vendor/adminlte/es/adminlte.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Nombre completo',
+    'email'                       => 'Email',
+    'password'                    => 'Contraseña',
+    'retype_password'             => 'Confirmar la contraseña',
+    'remember_me'                 => 'Recordarme',
+    'register'                    => 'Registrarse',
+    'register_a_new_membership'   => 'Crear una nueva cuenta',
+    'i_forgot_my_password'        => '¿Olvidó su contraseña?',
+    'i_already_have_a_membership' => 'Ya tengo una cuenta',
+    'sign_in'                     => 'Ingresar',
+    'log_out'                     => 'Cerrar Sesión',
+    'toggle_navigation'           => 'Navegación de palanca',
+    'login_message'               => 'Iniciar Sesión',
+    'register_message'            => 'Crear una nueva cuenta',
+    'password_reset_message'      => 'Restablecer la contraseña',
+    'reset_password'              => 'Restablecer la contraseña',
+    'send_password_reset_link'    => 'Enviar enlace de restablecimiento de contraseña',
+    'username'                    => 'Nombre de Usuario',  
+];
